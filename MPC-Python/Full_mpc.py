98d5214c562d1c72e51a5dfeb74e7047f9a1c7c7
Larry-LL <lus16@seas.upenn.edu>
2024-12-04 20:04:00 -0500

    updated the full_mpc file

--- MPC-Python/Full_mpc.py
+++ MPC-Python/Full_mpc.py
@@ -6,8 +6,6 @@ from scipy.optimize import minimize
 from matplotlib.animation import FuncAnimation
 from matplotlib.patches import Circle
 
-
-
 class BallbotMPC_DO:
     def __init__(self, Q, R, Qf, nx, nu, u_min, u_max):
         self.Q = Q   # State cost 
@@ -19,9 +17,27 @@ class BallbotMPC_DO:
         self.umax = u_max  # Maximum input constraint
         self.N = 15  # Prediction horizon
         self.T = 0.1  # Time step
-        self.tilt_angle_limit = 0.174533  #theta x and y tilt angle limitation 
+        self.tilt_angle_limit = 0.349  #theta x and y tilt angle limitation 
 
-    def dynamic_obs_traj(self,t_tot, num_steps):
+    # def dynamic_obs_traj(self,t_tot, num_steps):
+    #     t = np.linspace(0,t_tot,num_steps)
+    #     #obs1 setup 
+    #     amplitude = 1
+    #     frequency = 0.5
+
+    #     obs1_x = amplitude * np.sin(2 * np.pi * frequency * t) + 1  # Moves back and forth around x=1
+    #     obs1_y = 1.5 * np.ones(num_steps)  # Constant y-coordinate
+    #     obs1_traj = np.vstack((obs1_x,obs1_y))
+
+    #     #obs2 setup
+    #     obs2_x = amplitude * np.sin(2 * np.pi * frequency * t) + 3 # Moves back and forth around x=1
+    #     obs2_y = 1.5 * np.ones(num_steps)  # Constant y-coordinate 
+    #     obs2_traj = np.vstack((obs2_x,obs2_y))
+        
+
+    #     return obs1_traj, obs2_traj
+    def dynamic_obs_traj(self,t_tot):
+        num_steps = int(t_tot / self.T) + 1
         t = np.linspace(0,t_tot,num_steps)
         #obs1 setup 
         amplitude = 1
@@ -35,9 +51,9 @@ class BallbotMPC_DO:
         obs2_x = amplitude * np.sin(2 * np.pi * frequency * t) + 3 # Moves back and forth around x=1
         obs2_y = 1.5 * np.ones(num_steps)  # Constant y-coordinate 
         obs2_traj = np.vstack((obs2_x,obs2_y))
+        
 
         return obs1_traj, obs2_traj
-
 
 
     def calcaulte_continous_dynamics(self,x_current,u_current):  # linearize at referenced state x_current & u_current
@@ -132,7 +148,7 @@ class BallbotMPC_DO:
         return Ad, Bd
     #return A_d, B_d 
     
-    def compute_mpc(self, x_current, u_current, x_goal, obs_radius,obs1_traj, obs2_traj):
+    def compute_mpc(self, x_current, u_current, x_ref, obs_radius,obs1_traj, obs2_traj):
 
         # Prediction horizon and dimensions
         N = self.N
@@ -162,11 +178,18 @@ class BallbotMPC_DO:
             # Extract control inputs and states from z
             u = z[: (N - 1) * nu].reshape(N - 1, nu)
             x = z[(N - 1) * nu :].reshape(N, nx)
+        
 
+            safe_distance = 0.5
+            obstacle_weight = 20
             cost = 0
             for i in range(N - 1):
+                obs1_x, obs1_y = obs1_traj[:, i]
+                distance_to_obs = ((x[i, 1] - obs1_x)**2 + (x[i, 5] - obs1_y)**2)
                 cost += (x[i] - x_goal).T @ self.Q @ (x[i] - x_goal) + u[i].T @ self.R @ u[i]
-            #termnial cost
+                if distance_to_obs < safe_distance**2:
+                    cost += obstacle_weight / distance_to_obs
+                            #termnial cost
             cost += (x[N - 1] - x_goal).T @ self.Qf @ (x[N - 1] - x_goal)
             return cost
 
@@ -192,7 +215,7 @@ class BallbotMPC_DO:
             return (x[0] - x_current).flatten()
         
         # obstacle avoidance constraint
-        def obstacle_avoidance_constraint(z):
+        def obstacle_avoidance_constraint(z,obs1_traj, obs2_traj):
             x = z[(N - 1) * nu :].reshape(N, nx)
             constraints = []
             for i in range(N):
@@ -200,13 +223,20 @@ class BallbotMPC_DO:
                 obs1_x, obs1_y = obs1_traj[:, i]
                 # print("x",x[i, 1].shape)
                 constraints.append(
-                    (x[i, 1] - obs1_x)**2 + (x[i, 5] - obs1_y)**2 - (obs_radius+0.05)**2
-                )
+                    (x[i, 1] - obs1_x)**2 + (x[i, 5] - obs1_y)**2 - (obs_radius+0.1)**2)
                 # Obstacle 2 avoidance
-                obs2_x, obs2_y = obs2_traj[:, i]
-                constraints.append(
-                    (x[i, 1] - obs2_x)**2 + (x[i, 5] - obs2_y)**2 - (obs_radius+0.05)**2
-                )
+                # obs2_x, obs2_y = obs2_traj[:, i]
+                # constraints.append(
+                #     (x[i, 1] - obs2_x)**2 + (x[i, 5] - obs2_y)**2 - (obs_radius+0.05)**2
+                # )
+            return np.array(constraints)
+        def input_sat_cons(z):
+            u = z[: (N - 1) * nu].reshape(N - 1, nu)
+            constraints = []
+            for i in range(N-1):
+                for j in range(nu):
+                    constraints.append(self.umax - u[i,j])
+                    constraints.append(u[i,j] - self.umin)
             return np.array(constraints)
         # state constraints like the tilt angle 
 
@@ -221,15 +251,19 @@ class BallbotMPC_DO:
             return np.array(constraints)
 
         # Combine constraints
+
         constraints = [
             {"type": "eq", "fun": dynamic_constraint},
             {"type": "eq", "fun": initial_constraint},
-            {"type": "ineq", "fun": obstacle_avoidance_constraint},
+            # {"type": "ineq", "fun": obstacle_avoidance_constraint, "args": (obs1_traj, obs2_traj)},
+            {"type": "ineq", "fun": input_sat_cons},
             {"type": "ineq", "fun": tilt_angle_constraint},
         ]
 
         # Solve optimization problem
-        result = minimize(cost_function, z0, bounds=bounds, constraints=constraints)
+        result = minimize(cost_function, z0, bounds=bounds, constraints=constraints,options={'maxiter': 1000, 'disp': True})
+        if not result.success:
+            print("Optimization failed:", result.message)
 
         # Extract optimal control inputs and states
         u_opt = result.x[: (N - 1) * nu].reshape(N - 1, nu)
@@ -240,20 +274,19 @@ class BallbotMPC_DO:
 
         return x_current, u_current
 
-        
 
-Q = np.diag([10, 100, 10, 10, 10, 100, 10, 10])
+
+Q = np.diag([10, 50, 5, 5, 10, 50, 5, 5])
 R = np.diag([5,5])
-Qf = np.diag([8, 100, 8, 10, 8, 100, 8, 10])
-x_goal = np.array([0, 4, 0, 0, 0, 3, 0, 0])
-x_current = np.zeros((8, 1))
+Qf = np.diag([8, 5, 8, 10, 8, 5, 8, 10])
+x_goal = np.array([0, 1.5, 0, 0, 0, 2, 0, 0])
+x_current = np.zeros((8, 1)).flatten()
 nx = 8
 nu = 2
-u_min = -4.9
-u_max = 4.9
+u_min = -5
+u_max = 5
 tolerance =0.5
-t_tot = 10
-num_steps = 100
+
 u_current = np.zeros((nu))
 
 ballbot_mpc = BallbotMPC_DO(Q, R, Qf, nx, nu, u_min, u_max)
@@ -262,9 +295,11 @@ iteration = 0
 x_pos = [0]
 y_pos = [0]
 
-obs1_traj, obs2_traj = ballbot_mpc.dynamic_obs_traj(t_tot, num_steps)
+t_tot = 10
 
-obs_radius = 0.3
+obs1_traj, obs2_traj = ballbot_mpc.dynamic_obs_traj(t_tot)
+
+obs_radius = 0.2
 
 error = 5
 
@@ -273,10 +308,18 @@ u1_list = [0]
 u2_list = [0]
 thetay_positions = [0]
 thetax_positions = [0]
+obs_look_ahead = 0
+
+x_current_ref = np.array([0, x_current[1], 0, 0, 0, x_current[5], 0, 0])
+
+x_ref_seg = np.linspace(x_current.flatten(), x_goal, 20)
+
 while error >= tolerance:
-# while iteration <= 50:
-    obs1_traj_slice = obs1_traj[:,iteration:iteration+ ballbot_mpc.N]
-    obs2_traj_slice = obs2_traj[:,iteration:iteration+ ballbot_mpc.N]
+# while iteration < 19:
+    obs1_traj_slice = obs1_traj[:,iteration+obs_look_ahead:iteration + ballbot_mpc.N+obs_look_ahead]
+    obs2_traj_slice = obs2_traj[:,iteration+obs_look_ahead:iteration+ ballbot_mpc.N+obs_look_ahead]
+    x_ref = np.linspace(x_ref_seg[iteration], x_ref_seg[iteration+1],ballbot_mpc.N)
+
     x_current, u_current = ballbot_mpc.compute_mpc(x_current, u_current, x_goal,obs_radius,obs1_traj_slice, obs2_traj_slice)
     x_pos.append(x_current[1])
     y_pos.append(x_current[5])
